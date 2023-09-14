affa617619b753d05a981c0138375e212adb644c
Michael Kreil <github@michael-kreil.de>
2023-09-14 16:13:34 +0200

    don't write temporary files

--- index.js
+++ index.js
@@ -2,72 +2,91 @@
 
 import Metalsmith from 'metalsmith';
 import 'colors';
+import 'work-faster';
 import markdown from '@metalsmith/markdown'
-import { mkdtempSync, readFileSync, readdirSync, rmSync, statSync } from 'node:fs';
-import { dirname, join, resolve } from 'node:path';
+import { dirname, join } from 'node:path';
 import { closest } from 'fastest-levenshtein';
 
-const directory = process.cwd();
 
-const tempDirectory = mkdtempSync('check-markdown-links-');
+let errorCount = 0;
+
+const directory = process.cwd();
+const tempDirectory = 'temp-check-markdown-links';
 
 const metalsmith = new Metalsmith(directory);
 metalsmith.source('.');
+metalsmith.clean();
+metalsmith.ignore([['.*', '.git']]);
 metalsmith.destination(tempDirectory);
 metalsmith.use(markdown({ engineOptions: { pedantic: false, gfm: true } }))
-await metalsmith.build();
 
-await check_html(tempDirectory);
+let files = await metalsmith.process();
+files = Array.from(Object.entries(files)).map(f => {
+	f[1].name = f[0];
+	return f[1]
+});
+check_html(files);
 
-rmSync(resolve(directory, tempDirectory), { recursive: true, force: true, maxRetries: 5, retryDelay: 100 })
+if (errorCount === 0) {
+	process.exit(0);
+} else {
+	process.exit(1);
+}
 
-async function check_html(directory) {
-	const files = [];
-	list_files_rec('');
-
+async function check_html(files) {
 	const linksKnown = new Set();
 	const linksExt = new Map();
 	const linksInt = new Map();
-	files.forEach(f => {
-		if (f.isHtml) {
-			let filename = f.rel.replace(/\.html?$/i, '.md');
-			linksKnown.add(filename);
+	files.forEach(file => {
+		if (file.name.startsWith('.')) return;
 
-			let html = readFileSync(f.abs, 'utf8');
+		if (file.name.endsWith('.html')) {
+			let orig_filename = file.name.replace(/\.html?$/i, '.md');
+			linksKnown.add(orig_filename);
+
+			let html = file.contents.toString('utf8');
 
 			for (let m of html.matchAll(/ id=\"(.*?)\"/g)) {
-				linksKnown.add(filename + '#' + m[1]);
+				linksKnown.add(orig_filename + '#' + m[1]);
 			}
 
 			for (let m of html.matchAll(/ (href|src)=\"(.*?)\"/g)) {
 				m = decodeURIComponent(m[2]);
 				if (m.startsWith('http://') || m.startsWith('https://')) {
-					addLinkOut(m, filename, true)
+					addLinkOut(m, orig_filename, true)
 				} else if (m.startsWith('#')) {
-					addLinkOut(filename + m, filename)
+					addLinkOut(orig_filename + m, orig_filename)
 				} else {
-					addLinkOut(join(dirname(filename), m), filename)
+					addLinkOut(join(dirname(orig_filename), m), orig_filename)
 				}
 			}
 		} else {
-			linksKnown.add(f.rel);
+			linksKnown.add(file.name);
 		}
 	})
 
 	for (let [link, filenames] of linksInt.entries()) {
 		if (linksKnown.has(link)) continue;
-		console.error(`Unknown link found: ${link}`.red);
+		console.error(`\nUnknown link found: ${link}`.red);
 		console.error('   Used in: ' + filenames.join(', '));
 		console.error('   Maybe you mean: ' + closest(link, Array.from(linksKnown.values())).white);
+		errorCount += 1;
 	}
- 
-	for (let [link, filenames] of entries) {
+
+	let entries = Array.from(linksExt.entries());
+	console.log(`checking ${entries.length} external links`)
+	await entries.forEachAsync(async ([link, filenames]) => {
+		let time = Date.now();
+
 		let response = await fetch(link);
-		if (response.status === 200) continue;
+
+		console.log(link, Date.now() - time);
+		if (response.status === 200) return;
 		console.error(`External link unreachable: ${link}`.red);
 		console.error('   Used in: ' + filenames.join(', '));
 		console.error('   Got status: ' + response.status);
-	}
+		errorCount += 1;
+	})
 
 
 	function addLinkOut(href, filename, external) {
@@ -87,18 +106,4 @@ async function check_html(directory) {
 		}
 	}
 
-	function list_files_rec(d) {
-		let path = resolve(directory, d);
-		readdirSync(path).forEach(name => {
-			if (name.startsWith('.')) return;
-
-			let abs = resolve(path, name);
-			let rel = join(d, name);
-			if (statSync(abs).isDirectory()) {
-				list_files_rec(rel);
-			} else {
-				files.push({ rel, abs, isHtml: name.endsWith('.html') })
-			}
-		})
-	}
 }
