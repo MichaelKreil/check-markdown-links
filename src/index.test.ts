76bae35da82507853e8c0e9816503ab9e0893328
Michael Kreil <github@michael-kreil.de>
2025-12-19 16:28:50 +0100

    test: fix line numbers in error messages for clarity

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,11 +9,11 @@ describe('run on test_data', () => {
 		const { errors } = result;
 		expect(errors.length).toBe(3);
 		expect(errors[0].toString()).toBe(
-			'Unknown link found "sub/doc2.md#header-1" in doc1.md (L:8), sub/doc2.md (L:4,6)'
+			'Unknown link found "sub/doc2.md#header-1" in doc1.md (L:9), sub/doc2.md (L:4,6)'
 		);
-		expect(errors[1].toString()).toBe('Unknown link found "doc1.md#header-3" in doc1.md (L:9), sub/doc2.md (L:5)');
+		expect(errors[1].toString()).toBe('Unknown link found "doc1.md#header-3" in doc1.md (L:10), sub/doc2.md (L:5)');
 		expect(errors[2].toString()).toBe(
-			'External link unreachable "https://this-domain-would-only-be-bought-by-someone-who-is-bored-and.rich" in doc1.md (L:12)'
+			'External link unreachable "https://this-domain-would-only-be-bought-by-someone-who-is-bored-and.rich" in doc1.md (L:13)'
 		);
 	});
 });
